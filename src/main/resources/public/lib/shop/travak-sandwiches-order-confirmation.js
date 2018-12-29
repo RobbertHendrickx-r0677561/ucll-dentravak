d5dd176b05caed059339f1abd99f1c6c816b1156
LAPTOP-H4FA1527\Thijs Vercammen <thijs.vercammen@.student.ucll.be>
2018-12-29 14:20:46 +0100

    small change confirmation page

--- src/main/resources/public/lib/shop/travak-sandwiches-order-confirmation.js
+++ src/main/resources/public/lib/shop/travak-sandwiches-order-confirmation.js
@@ -18,7 +18,7 @@ class DenTravakSandwichesOrderConfirmation extends DenTravakAbstractElement {
             .forEach(scoreBtn => scoreBtn.addEventListener('click', e => {
 
                 let recommendedItem = {};
-                recommendedItem.emailAddress = this.order.phoneNumber;
+                recommendedItem.emailAddress = this.order.mobilePhoneNumber;
                 recommendedItem.ratedItem = this.order.sandwichId;
                 recommendedItem.rating = scoreBtn.dataset.score;
 
